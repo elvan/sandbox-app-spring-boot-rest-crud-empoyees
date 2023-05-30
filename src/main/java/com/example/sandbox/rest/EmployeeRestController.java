3bb3e07ab6b2ab9a82b689049934dfc77286294b
Elvan Hidayat <elvanhd@gmail.com>
2023-05-30 10:43:43 +0700

    update an employee

--- src/main/java/com/example/sandbox/rest/EmployeeRestController.java
+++ src/main/java/com/example/sandbox/rest/EmployeeRestController.java
@@ -8,6 +8,7 @@ import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -51,6 +52,16 @@ public class EmployeeRestController {
         // this is to force a save of new item ... instead of update
 
         theEmployee.setId(0);
+
+        Employee dbEmployee = employeeService.save(theEmployee);
+
+        return dbEmployee;
+    }
+
+    // add mapping for PUT /employees - update existing employee
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee theEmployee) {
 
         Employee dbEmployee = employeeService.save(theEmployee);
 
